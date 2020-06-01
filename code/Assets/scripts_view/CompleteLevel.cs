0bf1fe6723c03426563dfe4538ad5e08e4b98e86
Zaptopod4001 <32018324+Zaptopod4001@users.noreply.github.com>
2020-06-01 13:51:15 +0300

    Update CompleteLevel.cs

--- code/Assets/scripts_view/CompleteLevel.cs
+++ code/Assets/scripts_view/CompleteLevel.cs
@@ -2,6 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Copyright Sami S.
+
+// use of any kind without a written permission 
+// from the author is not allowed.
+
+// DO NOT:
+// Fork, clone, copy or use in any shape or form.
+
+
 public class CompleteLevel : MonoBehaviour
 {
     
